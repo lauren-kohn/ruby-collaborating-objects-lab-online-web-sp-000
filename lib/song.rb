21c6f5860bb3d8e5cefafc079fe76de62c3dd14d
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 22:23:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song 
   
   attr_accessor :name, :artist
@@ -23,6 +25,7 @@ class Song
   end
   
   def artist_name=(name)
+    binding.pry
     Artist.find_or_create_by_name(name)
     #if Artist.all.detect { |artist| artist == name }
     #  self.artist = Artist.new(name)
