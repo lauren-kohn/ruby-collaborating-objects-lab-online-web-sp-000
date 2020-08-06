993698979f0a9b79bc51af48d3869e2a3be7a58f
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 21:46:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     split_name = filename.split(" - ")
     song = self.new(split_name[1])
     artist = Artist.find_or_create_by_name(split_name[0])
-    song.artist = 
+    song.artist = artist
   end
   
   def artist_name=(name)
