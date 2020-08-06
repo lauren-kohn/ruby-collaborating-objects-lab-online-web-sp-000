bb45bfc3ce25ea6f9936809e36e99897746fd217
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 22:44:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,12 +3,14 @@ class MP3Importer
   attr_accessor :path
   
   def initialize(filename)
-    @path = filename
+    @path = filename # associates the given data with the @path instance variable
   end 
   
   def files 
     @files = Dir.entries(@path)
+      # loads the mp3 files from the directory and normalizes the filename
     @files.delete_if {|file| file == "." || file == ".."}
+      # removes unnecessary data
   end
   
   def import
