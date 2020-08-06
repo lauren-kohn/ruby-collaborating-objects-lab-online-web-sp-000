d4ae893779b3d67fefd0cb06e6cad4187596dca9
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 20:36:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,11 @@ class Artist
   #end 
   
   def self.find_or_create_by_name(name)
-    
+    if @@all.include?(name)
+      puts name
+    else
+      Artist.new(name)
+    end
   end
   
 #  def print_songs 
