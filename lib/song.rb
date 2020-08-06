b5204fa259958eb51a26abe78599628d1b022166
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 21:31:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,11 @@ class Song
   #end
   
  # def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else 
-      self.artist.name = name 
-    end 
-  end 
+ #   if (self.artist.nil?)
+#      self.artist = Artist.new(name)
+#    else 
+ #     self.artist.name = name 
+#    end 
+#  end 
 
 end
