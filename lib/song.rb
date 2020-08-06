abae0fe95b16625aab5efdeeee2c9a1115ee0b79
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 22:20:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,11 +23,11 @@ class Song
   end
   
   def artist_name=(name)
-    if Artist.all.detect { |artist| artist == name }
-      self.artist = Artist.new(name)
-    else 
-      self.artist.name = name 
-    end 
+    #if Artist.all.detect { |artist| artist == name }
+    #  self.artist = Artist.new(name)
+    #else 
+    #  self.artist.name = name 
+    #end 
   end 
 
 end
