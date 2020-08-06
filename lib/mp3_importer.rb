f33aee3b46ff7c14f6a7bdd4079e2c411f7400fe
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-06 20:30:11 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer 
+  
+  attr_accessor :path
+  
+  def initialize(filename)
+    @path = filename
+  end 
+  
+  def files 
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+  
+  def import(list_of_filenames)
+    list_of_filenames.each { |filename| song.new_by_filename(filename) }
+  end 
+  
+end
